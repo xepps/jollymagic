fdef475d8f62e47ca12c0293788c06ec9dca3909
Kristian Epps <kristian.epps@bbc.co.uk>
2015-06-14 19:15:02 +0100

    Grunt set up and working

---
+++ app/assets/scss/compass_config.rb
@@ -0,0 +1,14 @@
+require 'json'
+
+http_path = '/'
+css_path = 'static/css'
+css_dir = 'app/assets/scss'
+sass_path = 'app/assets/scss'
+images_dir = 'app/assets/images'
+generated_images_dir = 'static/images'
+http_generated_images_path = '../../images'
+output_style = :expanded
+relative_assets = false
+line_comments = true
+
+sprite_load_path = ['app/assets/images']
