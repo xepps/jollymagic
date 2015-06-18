76b0253debfe6acdcfff36128160dc400929e9e3
Kristian Epps <kristian.epps@bbc.co.uk>
2015-06-18 22:06:35 +0100

    current prototype

--- app/lib/views/index.php
+++ app/lib/views/index.php
@@ -5,35 +5,35 @@
 </head>
 <body>
 <div class="wrapper">
-    <div class="nav">
-        <ul>
-            <li>
-                <a>Mr Jolly</a>
-            </li>
-            <li>
-                <a>Safeguarding Children</a>
-            </li>
-            <li>
-                <a>Close up Magic</a>
-            </li>
-            <li>
-                <a>Balloon Modelling</a>
-            </li>
-            <li>
-                <a>Casino Nights</a>
-            </li>
-            <li>
-                <a>Booking Form</a>
-            </li>
-        </ul>
-    </div>
     <div class="page-background">
         <div class="page-overlay">
-            <div class="header">
-                <img src="<?= $baseUrl ?>/image/mr_jolly.png">
+            <div class="nav">
+                <div class="logo">
+                    <img src="<?= $baseUrl ?>/image/mr_jolly.png">
+                </div>
+                <ul>
+                    <li>
+                        <a>Mr Jolly</a>
+                    </li>
+                    <li>
+                        <a>Safeguarding Children</a>
+                    </li>
+                    <li>
+                        <a>Close up Magic</a>
+                    </li>
+                    <li>
+                        <a>Balloon Modelling</a>
+                    </li>
+                    <li>
+                        <a>Casino Nights</a>
+                    </li>
+                    <li>
+                        <a>Booking Form</a>
+                    </li>
+                </ul>
             </div>
             <div class="body">
-                <h2 class="page-title">Mr Jolly's Children's Magic Show!</h2>
+                <h2 class="page-title">Hi, I'm Alan Jolly!</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac sem vehicula, vestibulum risus eu, porttitor libero. Morbi aliquam tellus a lectus mattis finibus. Pellentesque fermentum vel justo ut finibus.</p>
                 <p>Etiam posuere purus nec dui luctus, vitae tempor tellus convallis. Fusce mattis, lectus sed sodales vulputate, ante magna congue ligula, id viverra nulla dui in est.</p>
                 <p>Sed lacus tellus, ornare sit amet suscipit sed, viverra id mi. Sed ac risus vitae dolor maximus congue quis eleifend felis.</p>
