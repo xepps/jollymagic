902b56add31690ee9d2679800a6d8deadfd4f164
Kristian Epps <kristian@xepps.com>
2015-09-10 09:07:56 +0100

    Update page title as per request

--- app/lib/views/index.php
+++ app/lib/views/index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alan Jolly's Jollymagic.com</title>
+    <title>Al Jolly's Jollymagic.com</title>
     <link rel="stylesheet" type="text/css" href="/static/css/main.css">
 </head>
 <body>
