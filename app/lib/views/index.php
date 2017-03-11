7972d7018f5cb12a39f58b90edc0181ac4545e6c
Kristian Epps <kristian.epps@bbc.co.uk>
2017-03-11 18:23:38 +0000

    added social buttons

--- app/lib/views/index.php
+++ app/lib/views/index.php
@@ -65,6 +65,8 @@
                 ),
                 " ● "
             ) ?></p>
+        <a class="twitter-mention-button" href="https://twitter.com/intent/tweet?screen_name=Jollymagic&text=Please entertain us" data-size="large">Tweet @Jollymagic</a>
+        <div class="fb-follow" data-href="https://www.facebook.com/jollymagic" data-width="300" data-layout="standard" data-size="large" data-show-faces="true"></div>
     </footer>
 
     <meta itemprop="paymentAccepted" content="cash, cheque, bank transfer" />
@@ -89,6 +91,36 @@
         ga('send', 'pageview');
 
     </script>
+
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1756516937993502";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <script>
+        window.twttr = (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],t = window.twttr || {};
+            if (d.getElementById(id)) return t;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+
+            t._e = [];
+            t.ready = function(f) {
+                t._e.push(f);
+            };
+
+            return t;
+        }(document, "script", "twitter-wjs"));
+    </script>
+
 
 </body>
 </html>
