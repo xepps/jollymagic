26a5f265c66d2348dd5dd045c0732251c14e5635
Kristian Epps <kristian.epps@bbc.co.uk>
2017-03-11 21:57:06 +0000

    load the image from the correct location

--- app/assets/scss/compass_config.rb
+++ app/assets/scss/compass_config.rb
@@ -4,11 +4,11 @@ http_path = '/'
 css_path = 'static/css'
 css_dir = 'app/assets/scss'
 sass_path = 'app/assets/scss'
-images_dir = 'app/assets/images'
+images_dir = 'image'
 generated_images_dir = 'static/images'
 http_generated_images_path = '../../images'
 output_style = :expanded
 relative_assets = false
 line_comments = true
 
-sprite_load_path = ['app/assets/images']
+sprite_load_path = ['image']
